e913d904ed2c2fbd1f886239a099b8200db82168
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-09-13 19:31:00 +0200

    Add protocol version to each packet

--- eterm/src/lib.rs
+++ eterm/src/lib.rs
@@ -79,6 +79,21 @@ pub use server::{ClientId, Server};
 
 use std::sync::Arc;
 
+/// All TCP packets are prefixed with this.
+///
+/// b"eterm", major, minor, patch
+pub(crate) const PROTOCOL_HEADER: [u8; 8] = [b'e', b't', b'e', b'r', b'm', 0, 0, 1];
+
+#[test]
+fn test_version() {
+    let [_, _, _, _, _, major, minor, patch] = PROTOCOL_HEADER;
+    assert_eq!(
+        env!("CARGO_PKG_VERSION"),
+        format!("{}.{}.{}", major, minor, patch),
+        "You must update PROTOCOL_HEADER when you publish a new eterm",
+    );
+}
+
 pub type Packet = Arc<[u8]>;
 
 #[derive(Default)]
@@ -158,10 +173,10 @@ impl TcpEndpoint {
     fn try_receive_packet(&mut self) -> anyhow::Result<Option<Packet>> {
         use std::io::Read as _;
 
-        // All messages are length-prefixed by u32 (LE).
-        let mut length = [0_u8; 4];
-        match self.tcp_stream.peek(&mut length) {
-            Ok(4) => {}
+        // All messages are length-prefixed by PROTOCOL_HEADER and u32 (LE).
+        let mut header = [0_u8; 12];
+        match self.tcp_stream.peek(&mut header) {
+            Ok(12) => {}
             Ok(_) => {
                 return Ok(None);
             }
@@ -174,14 +189,32 @@ impl TcpEndpoint {
             }
         }
 
-        let length = u32::from_le_bytes(length) as usize;
+        let protocol = &header[..PROTOCOL_HEADER.len()];
+        let length = &header[PROTOCOL_HEADER.len()..];
+        let length = u32::from_le_bytes([length[0], length[1], length[2], length[3]]) as usize;
+
+        if protocol[0..5] != PROTOCOL_HEADER[0..5] {
+            anyhow::bail!("The other side is not eterm");
+        }
+
+        if protocol != PROTOCOL_HEADER {
+            anyhow::bail!(
+                "This side uses eterm {}.{}.{}, the other side is on {}.{}.{}",
+                PROTOCOL_HEADER[5],
+                PROTOCOL_HEADER[6],
+                PROTOCOL_HEADER[7],
+                protocol[5],
+                protocol[6],
+                protocol[7],
+            );
+        }
 
         if length > 32_000_000 {
             anyhow::bail!("Refusing packet of {:.1} MB", length as f32 * 1e-6);
         }
 
         // See if we have the whole packet yet:
-        let mut length_and_packet = vec![0_u8; 4 + length];
+        let mut length_and_packet = vec![0_u8; header.len() + length];
         match self.tcp_stream.peek(&mut length_and_packet) {
             Ok(bytes_read) => {
                 if bytes_read != length_and_packet.len() {
@@ -200,7 +233,7 @@ impl TcpEndpoint {
         // consume the bytes:
         self.tcp_stream.read_exact(&mut length_and_packet)?;
 
-        let packet = &length_and_packet[4..];
+        let packet = &length_and_packet[header.len()..];
 
         Ok(Some(packet.into()))
     }
@@ -220,6 +253,7 @@ impl TcpEndpoint {
     fn send_packet(&mut self, packet: &[u8]) -> anyhow::Result<()> {
         let length = packet.len() as u32;
         let length = length.to_le_bytes();
+        self.write_all_with_retry(&PROTOCOL_HEADER)?;
         self.write_all_with_retry(&length)?;
         self.write_all_with_retry(packet)?;
         Ok(())
